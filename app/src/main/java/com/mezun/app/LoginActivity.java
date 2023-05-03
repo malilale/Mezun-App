c8c0d3e5b9ef6970cd4af9e3f14bcd5d3784e2d2
Muhammed Ali LALE <malilale66@gmail.com>
2023-05-03 20:39:09 +0300

    Password recovery

--- app/src/main/java/com/mezun/app/LoginActivity.java
+++ app/src/main/java/com/mezun/app/LoginActivity.java
@@ -2,13 +2,15 @@ package com.mezun.app;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Patterns;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -17,8 +19,6 @@ import com.google.firebase.auth.FirebaseUser;
 public class LoginActivity extends AppCompatActivity {
 
     private EditText et_email, et_password;
-    private Button btn_login;
-    private TextView tv_forgotPw, tv_register;
     private FirebaseAuth mAuth;
 
 
@@ -29,34 +29,58 @@ public class LoginActivity extends AppCompatActivity {
 
         et_email = findViewById(R.id.et_email);
         et_password = findViewById(R.id.et_password);
-        btn_login = findViewById(R.id.btn_login);
-        tv_forgotPw = findViewById(R.id.tv_forgotpw);
-        tv_register = findViewById(R.id.tv_register);
+        Button btn_login = findViewById(R.id.btn_login);
+        TextView tv_forgotPw = findViewById(R.id.tv_forgotpw);
+        TextView tv_register = findViewById(R.id.tv_register);
         mAuth = FirebaseAuth.getInstance();
 
-        btn_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String email = et_email.getText().toString().trim();
-                String password = et_password.getText().toString().trim();
-                if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-                    //error message
-                    et_email.setError(getString(R.string.invaild_email));
-                    et_email.setFocusable(true);
-                }else {
-                    login(email,password); //login user
-                }
-
+        btn_login.setOnClickListener(view -> {
+            String email = et_email.getText().toString().trim();
+            String password = et_password.getText().toString().trim();
+            if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                //error message
+                et_email.setError(getString(R.string.invaild_email));
+                et_email.setFocusable(true);
+            }else {
+                login(email,password); //login user
             }
 
         });
 
-        tv_register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
-                startActivity(intent);
-            }
+        tv_register.setOnClickListener(view -> {
+            Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+            startActivity(intent);
+        });
+
+        tv_forgotPw.setOnClickListener(view -> recoverPasswordDialog());
+    }
+
+    private void recoverPasswordDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LinearLayout layout = new LinearLayout(this);
+        final EditText et_dialog_email = new EditText(this);
+        et_dialog_email.setHint("E-Posta");
+        et_dialog_email.setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+
+        layout.addView(et_dialog_email);
+        layout.setPadding(10,10,10,10);
+
+        builder.setView(layout);
+
+        builder.setPositiveButton("Gönder", (dialogInterface, i) -> {
+            String email_dialog = et_dialog_email.getText().toString().trim();
+            recover(email_dialog);
+        });
+        builder.setNegativeButton("İptal", (dialogInterface, i) -> dialogInterface.dismiss());
+        builder.create().show();
+    }
+
+    private void recover(String email) {
+        mAuth.sendPasswordResetEmail(email).addOnCompleteListener(task -> {
+            if(task.isSuccessful())
+                Toast.makeText(LoginActivity.this, "E-Posta Gönderildi", Toast.LENGTH_SHORT).show();
+            else
+                Toast.makeText(LoginActivity.this, "E-Posta Gönderilemedi", Toast.LENGTH_SHORT).show();
         });
     }
 
