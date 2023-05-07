6fe6a35de265424f244ee52c4037209d7a358f75
Muhammed Ali LALE <malilale66@gmail.com>
2023-05-07 13:52:18 +0300

    Filtering users activity done

--- app/src/main/java/com/mezun/app/UsersFragment.java
+++ app/src/main/java/com/mezun/app/UsersFragment.java
@@ -2,17 +2,29 @@ package com.mezun.app;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 
@@ -22,6 +34,9 @@ public class UsersFragment extends Fragment implements SelectListener{
     UserAdapter userAdapter;
     ArrayList<User> list;
     CollectionReference reference;
+    private Button btn_menu,btn_filter;
+    private EditText et_filter;
+    private String option, filter;
 
 
     @Override
@@ -39,10 +54,77 @@ public class UsersFragment extends Fragment implements SelectListener{
         userAdapter = new UserAdapter(getActivity(),list,this);
         recyclerView.setAdapter(userAdapter);
 
+        et_filter = view.findViewById(R.id.et_filter);
+        btn_menu = view.findViewById(R.id.btn_menu_filter);
+        btn_filter = view.findViewById(R.id.btn_filter);
+
+        et_filter.setText("");
+        option = "";
+
+        btn_menu.setOnClickListener(view1 ->
+                showPopupMenu());
+
+        btn_filter.setOnClickListener(view1 ->
+                setFilters());
+
+
         getData();
-
-
         return view;
+    }
+
+    private void setFilters() {
+        filter = et_filter.getText().toString().trim();
+        if(filter.isEmpty() || option.isEmpty()){
+            Toast.makeText(getActivity(), "Bir filtre seçiniz", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        list.clear();
+        Query filterQuery = reference.whereEqualTo(option,filter);
+
+        filterQuery.get().addOnSuccessListener(queryDocumentSnapshots -> {
+            for(DocumentSnapshot d : queryDocumentSnapshots.getDocuments()){
+                list.add(d.toObject(User.class));
+            }
+            userAdapter.notifyDataSetChanged();
+        }).addOnFailureListener(e -> {
+            Toast.makeText(getActivity(), "Failed: "+e.getMessage(), Toast.LENGTH_SHORT).show();
+        });
+
+    }
+
+    private void showPopupMenu() {
+        PopupMenu popupMenu = new PopupMenu(getActivity(), btn_menu);
+        popupMenu.getMenuInflater().inflate(R.menu.filter_menu, popupMenu.getMenu());
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                option = getOption(menuItem.getTitle().toString());
+                et_filter.setText("");
+                Toast.makeText(getActivity(), option, Toast.LENGTH_SHORT).show();
+                btn_menu.setText(menuItem.getTitle().toString());
+                return true;
+            }
+        });
+        popupMenu.show();
+    }
+
+    private String getOption(String item) {
+
+        if (item.contains("Ülke")){
+            et_filter.setInputType(InputType.TYPE_CLASS_TEXT);
+            return "country";
+        }if (item.contains("Şehir")) {
+            et_filter.setInputType(InputType.TYPE_CLASS_TEXT);
+            return "city";
+        }if (item.contains("Giriş Yılı")) {
+            et_filter.setInputType(InputType.TYPE_CLASS_NUMBER);
+            return "startyear";
+        }if (item.contains("Mezun Yılı")) {
+            et_filter.setInputType(InputType.TYPE_CLASS_NUMBER);
+            return "endyear";
+        }
+
+        return null;
     }
 
 
